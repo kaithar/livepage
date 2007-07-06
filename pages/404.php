3c3ed016dbb819b3863186684b4b81a633f8efdf
Kaithar <noreply.committer@the-cell.co.uk>
2007-07-06 23:07:55 +0000

    - Adjust the 404 page to work with the upcoming code

--- pages/404.php
+++ pages/404.php
@@ -1,11 +1,16 @@
 <?php
+
+$page = Array();
+$page['page_key'] = $request;
+$page['page_title'] = "Oops, 404";
+
 $foo = "The requested page doesn't seem to exist.<br/><br/>";
 $foo .= "If you followed a link to get here, please inform the owner of the originating page that their link is broken.<br/><br/>";
 $foo .= "Perhaps you can find the desired page using the site navigation?";
 
 if ($user['editcontent'] == 1) 
 {
-  $foo .= "<br/><br/>Click <a href=\"$parent_path/$request.create\">here</a> to create this page.";
+  $foo .= "<br/><br/>Click <a href=\"$path_404.create\">here</a> to create this page.";
 }
 
 $content .= section("Ooops!",$foo);
