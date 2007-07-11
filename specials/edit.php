2d23e28fbb5ae76e5a1392505349480fac2345b2
Kaithar <noreply.committer@the-cell.co.uk>
2007-07-11 16:43:20 +0000

    Updating specials/edit.php to work with the new variables

--- specials/edit.php
+++ specials/edit.php
@@ -1,6 +1,6 @@
 <?php
 
-if ($_POST['Submit'] == "Submit")
+if (isset($_POST['Submit']) && ($_POST['Submit'] == "Submit") && isset($page['params'][1]))
 {
   $page_id = mysql_real_escape_string($page['page_id']);
 
@@ -8,24 +8,26 @@ if ($_POST['Submit'] == "Submit")
   $content = mysql_real_escape_string($_POST['content']);
 
   mysql_do_query("UPDATE `cms_sections` SET `section_title`='$title', `section_text`='$content' 
-                   WHERE `section_id`='".mysql_real_escape_string($vfile[2])."'");
-  header("location: ".$request);
+                   WHERE `section_id`='".mysql_real_escape_string($page['params'][1])."'");
+  header("location: ".$page['path']);
   die();
 }
 
 $page_id = mysql_real_escape_string($page['page_id']);
 
-$section = mysql_do_query("SELECT * FROM `cms_sections` 
-                            WHERE `page_id` = '$page_id' AND `section_id` = '".mysql_real_escape_string($vfile[2])."'");
+$section = mysql_do_query("SELECT *
+                             FROM `cms_sections` 
+                            WHERE `page_id` = '$page_id'
+                              AND `section_id` = '".mysql_real_escape_string($page['params'][1])."'");
 
 if (mysql_num_rows($section) == 0) {
-  header("location: ".$request);
+  header("location: ".$page['path']);
   die();
 }
 
 $section = mysql_fetch_assoc($section);
 
-$foo = "<form action=\"$request.edit.$vfile[2]\" method=\"POST\">";
+$foo = "<form action=\"{$page['path']}.edit.{$page['params'][1]}\" method=\"POST\">";
 $foo .= "Section title:<br><input type=\"text\" name=\"title\" size=\"95\" value=\"{$section['section_title']}\"><br><br>";
 $foo .= "Content:<br><textarea name=\"content\" cols=\"80\" rows=\"10\">{$section['section_text']}</textarea><br>";
 $foo .= "<input type=\"Submit\" name=\"Submit\" value=\"Submit\"></form>"; 
