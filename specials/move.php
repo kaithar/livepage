82d545e86180124a8aec83eb32fb5d61689ed538
Kaithar <noreply.committer@the-cell.co.uk>
2007-07-11 18:16:48 +0000

    Update move.php to work with the new code.  May require further adjustment to handle moving between categories.

--- specials/move.php
+++ specials/move.php
@@ -1,6 +1,6 @@
 <?
 
-if ($_POST['Submit'] == "Submit")
+if (isset($_POST['Submit']) && $_POST['Submit'] == "Submit")
 {
   $page_id = mysql_real_escape_string($page['page_id']);
 
@@ -15,12 +15,12 @@ if ($_POST['Submit'] == "Submit")
   {
     mysql_do_query("UPDATE `cms_pages` SET `page_key`='$key'
                      WHERE `page_id`='".mysql_real_escape_string($page_id)."'");
-    header("location: ".$key);
+    header("location: "$page['parent_path'].$key);
     die();
   }
 }
 
-$c = "<form action=\"$request.move\" method=\"POST\">";
+$c = "<form action=\"{$page['path']}.move\" method=\"POST\">";
 $c .= "Page title:<br><input type=\"text\" name=\"location\" size=\"95\" value=\"{$page['page_key']}\"><br>";
 $c .= "<input type=\"Submit\" name=\"Submit\" value=\"Submit\"></form>";
 
