5878b054ae34e8191bcec75e51dd28c2b2f60fcf
Kaithar <noreply.committer@the-cell.co.uk>
2007-07-11 15:55:47 +0000

    Updating code to work with new variables and category code

--- specials/delpage.php
+++ specials/delpage.php
@@ -1,19 +1,17 @@
-<?
-if ($vfile[2] == "CONFIRM")
+<?php
+
+if (isset($page['params'][1]) && isset($page['params'][2]) && ($page['params'][2] == "CONFIRM"))
 {
   $page_id = mysql_real_escape_string($page['page_id']);
 
   mysql_do_query("DELETE FROM `cms_sections` WHERE `page_id`='$page_id'");
   mysql_do_query("DELETE FROM `cms_pages` WHERE `page_id`='$page_id'");
-  mysql_do_query("UPDATE `cms_pages`
-                     SET `page_parent` = '".mysql_real_escape_string($page['page_parent'])."'
-                   WHERE `page_parent` = '$page_id'");
   
-  header("location: {$page['parent_url']}");
+  header("location: {$page['parent_path']}");
   die();
 }
 
-$content .= section("Delete PAGE...", "Are you sure you want to delete this PAGE? <a href=\"$request.delpage.CONFIRM\">YES</a> / <a href=\"$request\">NO</a>");
+$content .= section("Delete PAGE...", "Are you sure you want to delete this PAGE? <a href=\"{$page['path']}.delpage.CONFIRM\">YES</a> / <a href=\"{$page['path']}\">NO</a>");
 
 ?>
 
