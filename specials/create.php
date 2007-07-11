863036c0c48182a87c5535f124a6a84e39385923
Kaithar <noreply.committer@the-cell.co.uk>
2007-07-11 04:33:44 +0000

    Fix a minor typo on a table field, correct the redirect url, mass reindent

--- specials/create.php
+++ specials/create.php
@@ -2,31 +2,31 @@
 
 if ($page['found'] == false)
 {
-	$parent_id = 0;
+  $parent_id = 0;
   foreach ($path as $cat)
   {
     if ($cat['found'] == false)
-		{
-			$key = mysql_real_escape_string($cat['cat_key']);
+    {
+      $key = mysql_real_escape_string($cat['cat_key']);
       mysql_do_query("INSERT INTO `cms_categories`
                               SET `cat_parent` = '".mysql_real_escape_string($parent_id)."',
                                   `cat_key` = '$key',
                                   `cat_title` = '$key'");
-			// Note: this relies on there being no other queries after the insert.
-			// It'll probably break if there is.
-			$parent_id = mysql_insert_id();
-		}
-		else
-		{
-			$parent_id = $cat['cat_id'];
-		}
+      // Note: this relies on there being no other queries after the insert.
+      // It'll probably break if there is.
+      $parent_id = mysql_insert_id();
+    }
+    else
+    {
+      $parent_id = $cat['cat_id'];
+    }
   }
   mysql_do_query("INSERT INTO `cms_pages`
                           SET `page_key` = '".mysql_real_escape_string($page['page_key'])."',
-                              `page_parent` = '".mysql_real_escape_string($parent_id)."',
+                              `page_category` = '".mysql_real_escape_string($parent_id)."',
                               `page_title` = 'Under Construction'");
-  header("location: ".$request);
-	die();
+  header("location: ".$page['parent_path'].$page['page_key']);
+  die();
 }
 
 $showpage = 1;
