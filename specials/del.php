ec4eb2a56bf29a8186fe973f20a28ed46301eadc
Kaithar <noreply.committer@the-cell.co.uk>
2007-07-11 05:21:00 +0000

    Update del.php to work with the new variables.

--- specials/del.php
+++ specials/del.php
@@ -1,29 +1,32 @@
 <?
-if ($vfile[3] == "CONFIRM")
+if (isset($page['params'][1]) && isset($page['params'][2]) && ($page['params'][2] == "CONFIRM"))
 {
   $page_id = mysql_real_escape_string($page['page_id']);
 
   $section = mysql_do_query("SELECT * FROM `cms_sections`
                               WHERE `page_id` = '$page_id'
-                                AND `section_id` = '".mysql_real_escape_string($vfile[2])."'");
+                                AND `section_id` = '".mysql_real_escape_string($page['params'][1])."'");
 
   if (mysql_num_rows($section) == 0) {
-    header("location: ".$request);
+    header("location: ".$page['path']);
     die();
   }
 
   $section = mysql_fetch_assoc($section);
   
-  mysql_do_query("DELETE FROM `cms_sections` WHERE `section_id`='".mysql_real_escape_string($vfile[2])."' LIMIT 1");
-  mysql_do_query("UPDATE `cms_sections` SET `order` = `order` - 1
+  mysql_do_query("DELETE FROM `cms_sections` 
+                        WHERE `section_id`='".mysql_real_escape_string($page['params'][1])."'
+                        LIMIT 1");
+  mysql_do_query("UPDATE `cms_sections`
+                     SET `order` = `order` - 1
                    WHERE `page_id`='$page_id'
                      AND `order`>='".mysql_real_escape_string($section['order'])."'");
 
-  header("location: ".$request);
+  header("location: ".$page['path']);
   die();
 }
 
-$content .= section("Delete section...", "Are you sure you want to delete this section? <a href=\"$request.del.$vfile[2].CONFIRM\">YES</a> / <a href=\"$request\">NO</a>");
+$content .= section("Delete section...", "Are you sure you want to delete this section? <a href=\"{$page['path']}.del.{$page['params'][1]}.CONFIRM\">YES</a> / <a href=\"{$page['path']}\">NO</a>");
 
 ?>
 
