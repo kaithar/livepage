a6e0643e195ae23813dbbdcdb25894aa9012c671
Kaithar <noreply.committer@the-cell.co.uk>
2007-07-17 06:36:15 +0000

    Hopefully that should make move.php able to move files between categories (Bug 105)

--- specials/move.php
+++ specials/move.php
@@ -1,10 +1,16 @@
 <?
 
-if (isset($_POST['Submit']) && $_POST['Submit'] == "Submit")
+require_once('includes/cat_tree.php');
+
+$tree = build_cat_tree();
+
+if (isset($_POST['Submit']) && $_POST['Submit'] == "Submit" && isset($tree['ids'][$_POST['category']]))
 {
   $page_id = mysql_real_escape_string($page['page_id']);
 
   $key = mysql_real_escape_string($_POST['location']);
+	
+	$cat = mysql_real_escape_string($_POST['category']);
 
   $keytest = mysql_do_query("SELECT * FROM `cms_pages` WHERE `page_key`='$key'");
   if (mysql_num_rows($keytest) > 0)
@@ -13,14 +19,21 @@ if (isset($_POST['Submit']) && $_POST['Submit'] == "Submit")
   }
   else
   {
-    mysql_do_query("UPDATE `cms_pages` SET `page_key`='$key'
+    mysql_do_query("UPDATE `cms_pages` SET `page_key`='$key', `page_category`='$cat'
                      WHERE `page_id`='".mysql_real_escape_string($page_id)."'");
-    header("location: ".$page['parent_path'].$key);
+    header("location: ".$tree['ids'][$cat]['path']."/".$key);
     die();
   }
 }
 
 $c = "<form action=\"{$page['path']}.move\" method=\"POST\">";
+$c .= "<select name=\"category\">";
+foreach ($tree['flat'] as $tcat)
+{
+	$c .= "<option value=\"{$tcat['cat_id']}\"".(($tcat['cat_id'] == $page['page_category'])?" selected=\"selected\"":"").">";
+	$c .= $tcat['flat_path']."</option>";
+}
+$c .= "</select>";
 $c .= "Page title:<br><input type=\"text\" name=\"location\" size=\"95\" value=\"{$page['page_key']}\"><br>";
 $c .= "<input type=\"Submit\" name=\"Submit\" value=\"Submit\"></form>";
 
