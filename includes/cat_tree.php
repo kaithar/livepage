e3c15c42d0e9f7acc7810d33928605ec6ecd5857
Kaithar <noreply.committer@the-cell.co.uk>
2007-07-17 07:12:12 +0000

    Gah! Made the functions in cat_tree correctly pass by reference.

--- includes/cat_tree.php
+++ includes/cat_tree.php
@@ -2,22 +2,22 @@
 
 require_once('includes/db.php');
 
-function recursive_tree_path($flat, $node, $flat_path, $path)
+function recursive_tree_path(&$flat, &$node, $flat_path, $path)
 {
 	$flat_path .= $node['cat_title'];
 	$path .= $node['cat_key'];
 	$node['flat_path'] = $flat_path;
 	$node['path'] = $path;
-	$flat[] = $node;
+	$flat[] = &$node;
 	$flat_path .= " &raquo; ";
 	$path .= "/";
-	foreach ($node['children'] as $child)
-		recursive_tree_path($flat, $child, $flat_path, $path);
+	foreach ($node['children'] as $k => $v)
+		recursive_tree_path($flat, $node['children'][$k], $flat_path, $path);
 }
 
 function build_cat_tree()
 {
-  $tree = Array();
+	$tree = Array();
 	$tree['flat'] = Array();
 	$tree['ids'] = Array();
 	
@@ -28,12 +28,16 @@ function build_cat_tree()
 		$tree['ids'][$cat['cat_id']] = $cat;
 	}
 	
-	foreach ($tree['ids'] as $cat)
+	foreach ($tree['ids'] as $catid => $cat)
 	{
 		if ($cat['cat_parent'] == 0)
-			$tree['tree'] = $cat;
+		{
+			$tree['tree'] = &$tree['ids'][$catid];
+		}
 		else
-			$tree['ids'][$cat['cat_parent']]['children'] = $cat;
+		{
+			$tree['ids'][$cat['cat_parent']]['children'][] = &$tree['ids'][$catid];
+		}
 	}
 	
 	recursive_tree_path($tree['flat'],$tree['tree'],"","");
