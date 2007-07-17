11ead297ee1b8c2d1c1a7035f84a1800fb3f6f5a
Kaithar <noreply.committer@the-cell.co.uk>
2007-07-17 07:17:02 +0000

    Oops, would help if it allowed for the new category when checking for collisions (Bug #105)

--- specials/move.php
+++ specials/move.php
@@ -12,7 +12,7 @@ if (isset($_POST['Submit']) && $_POST['Submit'] == "Submit" && isset($tree['ids'
 	
 	$cat = mysql_real_escape_string($_POST['category']);
 
-  $keytest = mysql_do_query("SELECT * FROM `cms_pages` WHERE `page_key`='$key'");
+  $keytest = mysql_do_query("SELECT * FROM `cms_pages` WHERE `page_key`='$key' AND `page_category`='$cat'");
   if (mysql_num_rows($keytest) > 0)
   {
     $content .= "Sorry, that page is in use";
@@ -33,7 +33,7 @@ foreach ($tree['flat'] as $tcat)
 	$c .= "<option value=\"{$tcat['cat_id']}\"".(($tcat['cat_id'] == $page['page_category'])?" selected=\"selected\"":"").">";
 	$c .= $tcat['flat_path']."</option>";
 }
-$c .= "</select>";
+$c .= "</select> &raquo; ";
 $c .= "<input type=\"text\" name=\"location\" size=\"95\" value=\"{$page['page_key']}\"><br>";
 $c .= "<input type=\"Submit\" name=\"Submit\" value=\"Submit\"></form>";
 
