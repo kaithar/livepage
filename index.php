eb0fc86585b0707e914833c24ae577e966c2df4d
Kaithar <noreply.committer@the-cell.co.uk>
2009-01-25 00:42:14 +0000

    Warning clean up

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ if (preg_match("|^/lp-admin|", $requested))
 	// Cat 1 is / ... we need to say here that it's visible so that the menu is built.
 	$visible_categories = Array(1=>true);
 	// Other variables we need to fake...
-	$page = Array();
+	$page = Array("template_data"=>"", "page_title"=>"Admin Interface");
 	$path = Array();
 	require_once("specials/lp-admin.php");
 	$showpage = 0;
